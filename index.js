3a101507f4fa8a992fde2ac68435f978683ec8e8
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2022-08-27 14:34:53 +0700

    random words with port 5000

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const lowerWords = "qwertyuiopasdfghjklzxcvbnm";
+const upperWords = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+const words = lowerWords + upperWords;
+
+app.get("/api", (req, res) => {
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += words.charAt(Math.floor(Math.random() * 7));
+  }
+  res.status(200).json({ msg: `your words: ${result}` });
+});
+
+app.listen(PORT, () =>
+  console.log(`The program started on port http://localhost:${PORT}`)
+);
