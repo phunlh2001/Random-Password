5f77146db8654b5aefdb55998cb061e759f6f32d
Nguyen Le Hung Phu <phunlh2001@gmail.com>
2022-08-27 15:22:18 +0700

    add route /api/:number to generate length random words

--- index.js
+++ index.js
@@ -8,12 +8,32 @@ const upperWords = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
 const words = lowerWords + upperWords;
 
+/**
+ * [GET] get random words with 6 words
+ */
 app.get("/api", (req, res) => {
   let result = "";
   for (let i = 0; i < 6; i++) {
     result += words.charAt(Math.floor(Math.random() * words.length));
   }
   res.status(200).json({ msg: `your words: ${result}` });
+});
+
+/**
+ * [GET] get random words with a number someone put to url
+ */
+app.get("/api/:number", (req, res) => {
+  const length = parseInt(req.params.number); //parser param to integer
+
+  if (Number.isInteger(length) && length > 0) {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += words.charAt(Math.floor(Math.random() * words.length));
+    }
+    res.status(200).json({ msg: result });
+  } else {
+    res.status(400).json({ msg: "Only accept param is positive number" });
+  }
 });
 
 app.listen(PORT, () =>
